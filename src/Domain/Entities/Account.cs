5c76dce94cc69ee02fd71619c9e41e758cef33a1
vinnt060402 <vinnt060402@gmail.com>
2024-05-13 22:33:06 +0700

    [BeatSports][130524][2230][Add] Add more Entities

---
+++ src/Domain/Entities/Account.cs
@@ -0,0 +1,6 @@
+﻿namespace BeatSportsAPI.Domain.Entities;
+public class Account : BaseAuditableEntity
+{
+    public string UserName { get; set; } = null!;
+    public string Password { get; set; } = null!;
+}
---
+++ src/Domain/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BeatSportsAPI.Domain.Entities;
+public class User : BaseAuditableEntity
+{
+    [ForeignKey("Account")]
+    public Guid AccountId { get;set; }
+    public string? Email { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public string Gender { get; set; } = null!;
+    public string? ProfilePictureURL { get; set; }
+    public string? Bio { get; set; }
+    public string PhoneNumber { get; set; } = null!;
+    public string Role { get; set; } = null!;
+
+    public virtual Account Account { get; set; }
+}
