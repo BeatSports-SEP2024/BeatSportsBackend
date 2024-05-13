da02a72dca3e5fcf11bcc0c492a3c9d34cb353d5
vinnt060402 <vinnt060402@gmail.com>
2024-05-13 23:01:33 +0700

    [BeatSports][130524][2300][Add] Add Admin Entity

---
+++ src/Domain/Entities/Admin.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BeatSportsAPI.Domain.Entities;
+public class Admin : BaseAuditableEntity
+{
+    [ForeignKey("Account")]
+    public Guid AccountId { get; set; }
+
+    public virtual Account Account { get; set; }
+}
