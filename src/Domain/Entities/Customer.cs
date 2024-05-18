449744dd79e8c26475796925539a832ec2f27f6b
minhncn <minh27122002pro@gmail.com>
2024-05-18 22:25:46 +0700

    [BeatSports][18052024][Modify][Update DbContext for Customer field]

--- src/Domain/Entities/Customer.cs
+++ src/Domain/Entities/Customer.cs
@@ -6,6 +6,8 @@ public class Customer : BaseAuditableEntity
 {
     [ForeignKey("Account")]
     public Guid AccountId { get; set; }
+    public int RewardPoints { get; set; }
+    public string Address { get; set; }
 
     public virtual Account Account { get; set; }
     public virtual IList<BookingDetail> BookingDetails { get; set; }
--- src/Infrastructure/Persistence/BeatSportsAPIDbContext.cs
+++ src/Infrastructure/Persistence/BeatSportsAPIDbContext.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+﻿    using System.Reflection;
 using System.Reflection.Emit;
 using BeatSportsAPI.Application.Common.Interfaces;
 using BeatSportsAPI.Domain.Entities;
