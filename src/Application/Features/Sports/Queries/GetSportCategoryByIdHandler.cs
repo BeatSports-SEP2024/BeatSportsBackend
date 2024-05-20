f133a51cf3b106e8c227d0b8419cb3dbfeec9ced
minhncn <minh27122002pro@gmail.com>
2024-05-20 10:19:08 +0700

    [BeatSports][20052024][Add][Add get by id for sport category]

---
+++ src/Application/Features/Sports/Queries/GetSportCategoryByIdHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using BeatSportsAPI.Application.Common.Interfaces;
+using BeatSportsAPI.Application.Common.Response;
+using BeatSportsAPI.Application.Common.Exceptions;
+using MediatR;
+
+namespace BeatSportsAPI.Application.Features.Sports.Queries;
+public class GetSportCategoryByIdHandler : IRequestHandler<GetSportCategoryByIdCommand, SportCategoriesResponse>
+{
+    private readonly IBeatSportsDbContext _beatSportsDbContext;
+    private readonly IMapper _mapper;
+    public GetSportCategoryByIdHandler(IBeatSportsDbContext beatSportsDbContext, IMapper mapper)
+    {
+        _beatSportsDbContext = beatSportsDbContext;
+        _mapper = mapper;
+    }
+    public Task<SportCategoriesResponse> Handle(GetSportCategoryByIdCommand request, CancellationToken cancellationToken)
+    {
+        var isExistedItem = _beatSportsDbContext.SportsCategories
+            .Where(sc => sc.Id == request.SportCategoryId && !sc.IsDelete)
+            .ProjectTo<SportCategoriesResponse>(_mapper.ConfigurationProvider)
+            .SingleOrDefault();
+        if(isExistedItem == null)
+        {
+            throw new BadRequestException($"Do not find sport category with sport category ID: {request.SportCategoryId}");
+        }
+        return Task.FromResult(isExistedItem);
+    }
+}
