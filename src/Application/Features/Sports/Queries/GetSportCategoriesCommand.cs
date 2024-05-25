a27f32cfab42ed7262821035fec344f6529307da
minhncn <minh27122002pro@gmail.com>
2024-05-25 14:58:07 +0700

    [BeatSports][25052024][Add][Add Filter by Category for Sport Category]

--- src/Application/Features/Sports/Queries/GetSportCategoriesCommand.cs
+++ src/Application/Features/Sports/Queries/GetSportCategoriesCommand.cs
@@ -1,15 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+using AutoFilterer.Types;
 using BeatSportsAPI.Application.Common.Models;
 using BeatSportsAPI.Application.Common.Response;
+using BeatSportsAPI.Domain.Enums;
 using MediatR;
+using Newtonsoft.Json;
 
 namespace BeatSportsAPI.Application.Features.Sports.Queries;
 public class GetSportCategoriesCommand : IRequest<PaginatedList<SportCategoriesResponse>>
 {
     public int PageIndex { get; set; }
     public int PageSize { get; set; }
+    [EnumDataType(typeof(SportCategoriesEnums))]
+    public SportCategoriesEnums? SportCategoryName { get; set; }
 }
