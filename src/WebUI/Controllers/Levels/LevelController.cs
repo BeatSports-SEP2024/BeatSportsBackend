02d298c0e4155aaa58f1cadef24c2586eadf3821
minhncn <minh27122002pro@gmail.com>
2024-05-29 18:04:45 +0700

    [BeatSports][29052024][Add][CRUD Levels]

---
+++ src/WebUI/Controllers/Levels/LevelController.cs
@@ -0,0 +1,63 @@
+﻿using BeatSportsAPI.Application.Features.Rooms.Levels.Commands;
+using BeatSportsAPI.Application.Features.Rooms.Levels.Queries;
+using BeatSportsAPI.Application.Features.Sports.Commands;
+using BeatSportsAPI.Application.Features.Sports.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers.Levels;
+
+public class LevelController : ApiControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public LevelController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllLevel([FromQuery] GetAllLevelCommand request)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+        var response = await _mediator.Send(request);
+
+        return Ok(response);
+    }
+    [HttpPost]
+    public async Task<IActionResult> CreateNewLevel(CreateLevelCommand request)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+        var response = await _mediator.Send(request);
+
+        return Ok(response);
+    }
+    [HttpPut]
+    public async Task<IActionResult> UpdateLevel(UpdateLevelCommand request)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+        var response = await _mediator.Send(request);
+
+        return Ok(response);
+    }
+    [HttpDelete]
+    public async Task<IActionResult> DeleteLevel([FromQuery] DeleteLevelCommand request)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+        var response = await _mediator.Send(request);
+
+        return Ok(response);
+    }
+}
