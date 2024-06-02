44744c74320b7b994a358e23761900d4383b8781
minhncn <minh27122002pro@gmail.com>
2024-06-02 21:58:18 +0700

    [BeatSports][02062024][Modify][Update RoomMatchEntity]

--- src/Domain/Entities/Room/RoomMatch.cs
+++ src/Domain/Entities/Room/RoomMatch.cs
@@ -4,7 +4,7 @@ using BeatSportsAPI.Domain.Entities.CourtEntity;
 namespace BeatSportsAPI.Domain.Entities.Room;
 public class RoomMatch : BaseAuditableEntity
 {
-    public Guid CourtId { get; set; }
+    public Guid CourtSubdivisionId { get; set; }
     [ForeignKey("Level")]
     public Guid LevelId { get; set; }
     public TimeSpan StartTimeRoom { get; set; }
@@ -14,7 +14,7 @@ public class RoomMatch : BaseAuditableEntity
     public string? RuleRoom { get; set; }
     public string? Note { get; set; }
 
-    public virtual Court Court { get; set; } = null!;
+    public virtual CourtSubdivision CourtSubdivision { get; set; } = null!;
     public virtual Booking Booking { get; set; } = null!;
     public virtual Level Level { get; set; } = null!;
     public virtual IList<RoomMember> RoomMembers { get; set; } = null!;
