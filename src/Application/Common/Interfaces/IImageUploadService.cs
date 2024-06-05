71df36c924688745f7d698fae766eb656dbdd196
minhncn <minh27122002pro@gmail.com>
2024-06-05 23:49:11 +0700

    [BeatSports][05062024][Add][2348][Implement Firebase]

---
+++ src/Application/Common/Interfaces/IImageUploadService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeatSportsAPI.Application.Common.Interfaces;
+public interface IImageUploadService
+{
+    Task<string> UploadImage(string fileImg, string base64string);
+}
