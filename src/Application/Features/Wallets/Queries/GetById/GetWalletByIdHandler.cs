54692b80a8d02278aa92a6094dbd55c779a38385
minhncn <minh27122002pro@gmail.com>
2024-06-11 22:30:35 +0700

    [BeatSports][110624][2223][Add][Add api get all wallet & get by id]

---
+++ src/Application/Features/Wallets/Queries/GetById/GetWalletByIdHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using BeatSportsAPI.Application.Common.Exceptions;
+using BeatSportsAPI.Application.Common.Interfaces;
+using BeatSportsAPI.Application.Common.Response;
+using MediatR;
+
+namespace BeatSportsAPI.Application.Features.Wallets.Queries.GetById;
+public class GetWalletByIdHandler : IRequestHandler<GetWalletByIdCommand, WalletResponse>
+{
+    private readonly IBeatSportsDbContext _beatSportsDbContext;
+    private readonly IMapper _mapper;
+
+    public GetWalletByIdHandler(IBeatSportsDbContext beatSportsDbContext, IMapper mapper)
+    {
+        _beatSportsDbContext = beatSportsDbContext;
+        _mapper = mapper;
+    }
+
+    public Task<WalletResponse> Handle(GetWalletByIdCommand request, CancellationToken cancellationToken)
+    {
+        var wallet = _beatSportsDbContext.Wallets
+            .Where(w => w.Id == request.WalletId && !w.IsDelete)
+            .ProjectTo<WalletResponse>(_mapper.ConfigurationProvider)
+            .SingleOrDefault();
+        if(wallet == null)
+        {
+            throw new NotFoundException($"{request.WalletId} does not existed");
+        }
+        return Task.FromResult(wallet);
+    }
+}
