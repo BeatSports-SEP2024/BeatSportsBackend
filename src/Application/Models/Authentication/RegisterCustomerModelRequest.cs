a0b162ce087f479426f0b3563046531c8477a129
minhncn <minh27122002pro@gmail.com>
2024-06-16 17:36:05 +0700

    [BeatSports][160624][1735][Update][Update field for register new customer]

--- src/Application/Models/Authentication/RegisterCustomerModelRequest.cs
+++ src/Application/Models/Authentication/RegisterCustomerModelRequest.cs
@@ -23,8 +23,8 @@ public class RegisterCustomerModelRequest : IRequest<BeatSportsResponse>
     public string Password { get; set; } = null!;
     [EmailAddress]
     public string? Email { get; set; }
-    //public string? FirstName { get; set; }
-    //public string? LastName { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
     //public DateTime DateOfBirth { get; set; }
     //[EnumDataType(typeof(GenderEnums))]
     //public GenderEnums Gender { get; set; }
