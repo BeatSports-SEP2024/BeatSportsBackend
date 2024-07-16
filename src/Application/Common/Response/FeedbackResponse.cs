3ff3ff8206601e6865dc9345f6d33029eb703f9a
minhncn <minh27122002pro@gmail.com>
2024-07-16 20:17:25 +0700

    [BeatSports][160724][2016][Update][Allow list of feedback null when get court with feedback list]

--- src/Application/Common/Response/FeedbackResponse.cs
+++ src/Application/Common/Response/FeedbackResponse.cs
@@ -20,8 +20,8 @@ public class FeedbackResponse
 //For Detail Court screen
 public class FeedbackResponseV2
 {
-    public Guid CourtId { get; set; }
-    public decimal FeedbackStar { get; set; }
+    public Guid? CourtId { get; set; }
+    public decimal? FeedbackStar { get; set; }
     public string? FeedbackContent { get; set; }
     public string? ProfilePictureUrl { get; set; }
 }
