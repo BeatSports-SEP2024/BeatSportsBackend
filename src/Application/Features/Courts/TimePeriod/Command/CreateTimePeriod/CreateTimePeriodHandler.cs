b9f5871a9a54412a1eafa2a84685ecaeee729011
minhncn <minh27122002pro@gmail.com>
2024-07-19 01:02:11 +0700

    [BeatSports][190724][0101][Add][Add api for create, update timeperiod]

--- src/Application/Features/Courts/TimePeriod/Command/CreateTimePeriod/CreateTimePeriodHandler.cs
+++ src/Application/Features/Courts/TimePeriod/Command/CreateTimePeriod/CreateTimePeriodHandler.cs
@@ -1,4 +1,5 @@
-﻿using BeatSportsAPI.Application.Common.Exceptions;
+﻿using System;
+using BeatSportsAPI.Application.Common.Exceptions;
 using BeatSportsAPI.Application.Common.Interfaces;
 using BeatSportsAPI.Application.Common.Response;
 using BeatSportsAPI.Application.Common.Ultilities;
@@ -19,22 +20,32 @@ public class CreateTimePeriodHandler : IRequestHandler<CreateTimePeriodCommand, 
         var existedCourt = _beatSportsDbContext.Courts
             .Where(c => c.Id == request.CourtId && !c.IsDelete)
             .FirstOrDefault();
+
         if (existedCourt == null)
         {
             throw new NotFoundException($"{request.CourtId} is not existed");
         }
+
+        var timePeriods = _beatSportsDbContext.TimePeriods
+            .Where(tp => tp.CourtId == request.CourtId && !tp.IsDelete).ToList();
+
         foreach (var (start, end) in TimeUtils.GenerateTimeSlots(request.StartTime, request.EndTime))
         {
+            if (timePeriods.Any(tp => tp.StartTime < end && start < tp.EndTime))
+            {
+                throw new BadRequestException("Time period overlaps with existing period. Please check and try again.");
+            }
+
             var newTimePeriod = new Domain.Entities.CourtEntity.TimePeriod
             {
                 CourtId = request.CourtId,
-                //Description = request.Description,
                 StartTime = start,
-                EndTime = end,
-                //RateMultiplier = request.RateMultiplier
+                EndTime = end
             };
+
             await _beatSportsDbContext.TimePeriods.AddAsync(newTimePeriod, cancellationToken);
         }
+
         await _beatSportsDbContext.SaveChangesAsync(cancellationToken);
 
         return new BeatSportsResponse
