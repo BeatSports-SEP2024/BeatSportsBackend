f2afcdac122f00ec96348b86a1c04596230f784d
vinnt060402 <vinnt060402@gmail.com>
2024-07-19 19:46:24 +0700

    [BeatSports][190724][1945][Upadte] Update DistanceCalculation and PerfomanceBehaviour and Get List courts near by

--- Services/MapBox/DistanceCalculation.cs
+++ Services/MapBox/DistanceCalculation.cs
@@ -12,7 +12,7 @@ public class DistanceCalculation
 {
     public async Task<List<LocationDistance>> GetDistancesAsync(double latitude, double longtitude, List<Court> destinations)
     {
-        
+
         var locationList = destinations.ToList();
 
         //Tao Url de ket noi den MapBox
@@ -25,27 +25,31 @@ public class DistanceCalculation
 
         var distances = new List<LocationDistance>();
 
-        if (response.IsSuccessful)  
+        if (response.IsSuccessful)
         {
             //Tu response tra ve, lay ra cac element result ket qua tinh khoang cach
             var jsonResponse = JsonDocument.Parse(response.Content);
             var distancesArray = jsonResponse.RootElement.GetProperty("distances").EnumerateArray().First().EnumerateArray();
-            
+
             int index = 0;
-            
+
             foreach (var distanceElement in distancesArray)
             {
+                // Kiểm tra xem khoảng cách có hợp lệ không
+                if (distanceElement.ValueKind == JsonValueKind.Null)
+                    continue;
                 var distanceMeters = distanceElement.GetDouble();
 
                 //Khong hieu sao luon co 1 element bi thua, nen loai bo
-                if(distanceMeters == 0) {
+                if (distanceMeters == 0)
+                {
                     continue;
                 }
 
                 distances.Add(new LocationDistance
                 {
                     Location = locationList.ElementAt(index),
-                    DistanceInKm = Math.Round(distanceMeters/1000.0, 2) // Chuyen meters to kilometers, lam tron sau dau phay 2 chu so 
+                    DistanceInKm = Math.Round(distanceMeters / 1000.0, 2) // Chuyen meters to kilometers, lam tron sau dau phay 2 chu so 
                 });
                 index++;
             }
