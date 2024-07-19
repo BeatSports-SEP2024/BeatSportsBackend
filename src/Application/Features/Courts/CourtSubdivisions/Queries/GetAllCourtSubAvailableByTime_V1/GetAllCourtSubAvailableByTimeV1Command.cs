7e2fc8d5d8e63603747b9d3107de656e61d1e9a5
ducbinh02 <ducbinhnguyen.2002@gmail.com>
2024-07-20 01:43:44 +0700

    [BeatSports][200724][0143][Add][Add GetAllCourtSubByTime API]

---
+++ src/Application/Features/Courts/CourtSubdivisions/Queries/GetAllCourtSubAvailableByTime_V1/GetAllCourtSubAvailableByTimeV1Command.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BeatSportsAPI.Application.Features.Courts.CourtSubdivisions.Queries.GetCourtSubdivisionAndTimeByCourtIdAndDate;
+using BeatSportsAPI.Domain.Entities.CourtEntity;
+using MediatR;
+
+namespace BeatSportsAPI.Application.Features.Courts.CourtSubdivisions.Queries.GetAllCourtSubAvailableByTime_V1;
+public class GetAllCourtSubAvailableByTimeV1Command : IRequest<CourtSubCheckedResponse>
+{
+    public Guid CourtSubId { get; set; }
+    public DateTime DateCheck { get; set; }
+}
+
+public class CourtSubCheckedResponse 
+{
+    public Guid CourtSubId { get; set; }
+    public string CourtSubName { get; set; }
+    public List<TimeCheckingResponse> TimeBookeds { get; set; }
+}
+
+public class TimeCheckingResponse
+{
+    public Guid TimeCheckingId { get; set; }
+    public string StartTime { get; set; }
+    public string EndTime { get; set; }
+}
