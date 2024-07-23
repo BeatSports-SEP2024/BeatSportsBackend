9d0d5d505c0195afe71847017575869eb1b2ec61
ducbinh02 <ducbinhnguyen.2002@gmail.com>
2024-07-24 00:40:17 +0700

    [BeatSports][240724][0040][Update][Update JoinRoomMatch API]

--- src/Application/Features/Rooms/RoomRequests/Commands/CreateRoomRequests/CreateRoomRequestHandler.cs
+++ src/Application/Features/Rooms/RoomRequests/Commands/CreateRoomRequests/CreateRoomRequestHandler.cs
@@ -4,6 +4,7 @@ using BeatSportsAPI.Application.Common.Response;
 using BeatSportsAPI.Domain.Enums;
 using BeatSportsAPI.Domain.Entities.Room;
 using MediatR;
+using Microsoft.AspNetCore.Http.HttpResults;
 
 namespace BeatSportsAPI.Application.Features.Rooms.RoomRequests.Commands.CreateRoomRequests;
 public class CreateRoomRequestHandler : IRequestHandler<CreateRoomRequestCommand, BeatSportsResponse>
@@ -23,6 +24,51 @@ public class CreateRoomRequestHandler : IRequestHandler<CreateRoomRequestCommand
         var customer = _beatSportsDbContext.Customers
             .Where(c => c.Id == request.CustomerId && !c.IsDelete)
             .FirstOrDefault();
+
+        var roomMatchJoinedList = _beatSportsDbContext.RoomRequests
+                                .Where(x => x.CustomerId == customer.Id)
+                                .ToList();
+
+        if(roomMatchJoinedList.Count > 0)
+        {
+            var flag = 0;
+
+            foreach (var room in roomMatchJoinedList)
+            {
+
+                var roomMatchCheck = _beatSportsDbContext.RoomMatches
+                                .Where(x => x.Id == room.RoomMatchId)
+                                .FirstOrDefault();
+
+                if(roomMatch.StartTimeRoom.Date == roomMatchCheck.StartTimeRoom.Date)
+                {
+                    if (roomMatch.StartTimeRoom <= roomMatchCheck.StartTimeRoom && roomMatch.EndTimeRoom >= roomMatchCheck.EndTimeRoom)
+                    {
+                        flag++;
+                        break;
+                    }
+                    else if (((roomMatch.StartTimeRoom <= roomMatchCheck.StartTimeRoom) && (roomMatchCheck.StartTimeRoom <= roomMatchCheck.EndTimeRoom)))
+                    {
+                        flag++;
+                        break;
+                    }
+                    else if (((roomMatch.StartTimeRoom <= roomMatchCheck.EndTimeRoom) && (roomMatchCheck.EndTimeRoom <= roomMatchCheck.EndTimeRoom)))
+                    {
+                        flag++;
+                        break;
+                    }
+                }
+            }
+
+            if (flag > 0)
+            {
+                return Task.FromResult(new BeatSportsResponse
+                {
+                    Message = "400"
+                });
+            }
+
+        }
 
         if(roomMatch == null) 
         {
