fe3685d455476d474e004140b60c47ee1b46b335
chim347 <nhanntse160792@gmail.com>
2024-07-26 18:23:24 +0700

    [BeatSports][260724][1823][Add][Api get transaction with thirdParty]

---
+++ src/WebUI/Controllers/TransactionThirdparty/TransactionThirdpartyController.cs
@@ -0,0 +1,30 @@
+﻿using BeatSportsAPI.Application.Features.TransactionThridparty.Queries.GetTransactionByCusId;
+using BeatSportsAPI.Application.Features.Wallets.Queries.GetById;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers.TransactionThirdparty;
+[ApiController]
+public class TransactionThirdpartyController : ApiControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public TransactionThirdpartyController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    [Route("customerId")]
+    public async Task<IActionResult> GetTransactionThridpartyById([FromQuery] GetTransactionByCusIdCommand request)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+        var response = await _mediator.Send(request);
+
+        return Ok(response);
+    }
+}
