97b0d8089c5e310624928ecfdc212aa58d704bdb
minhncn <minh27122002pro@gmail.com>
2024-07-27 17:56:17 +0700

    [BeatSports][270724][1755][Add][Add redis for get booking detail before book]

--- src/Application/Features/Bookings/Queries/GetBookingDetailReadyForFinishBooking/GetBookingDetailReadyForFinishBookingQueryHandler.cs
+++ src/Application/Features/Bookings/Queries/GetBookingDetailReadyForFinishBooking/GetBookingDetailReadyForFinishBookingQueryHandler.cs
@@ -3,103 +3,132 @@ using BeatSportsAPI.Application.Common.Interfaces;
 using BeatSportsAPI.Domain.Entities.CourtEntity;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using StackExchange.Redis;
+using Services.Redis;
 
 namespace BeatSportsAPI.Application.Features.Bookings.Queries.GetBookingDetailReadyForFinishBooking;
 public class GetBookingDetailReadyForFinishBookingQueryHandler : IRequestHandler<GetBookingDetailReadyForFinishBookingQuery, BookingDetailReadyForFinishBookingResponse>
 {
     private readonly IBeatSportsDbContext _dbContext;
-
-    public GetBookingDetailReadyForFinishBookingQueryHandler(IBeatSportsDbContext dbContext)
+    private readonly IDatabase _database;
+    public GetBookingDetailReadyForFinishBookingQueryHandler(IBeatSportsDbContext dbContext, IDatabase database)
     {
         _dbContext = dbContext;
+        _database = database;
     }
 
     public async Task<BookingDetailReadyForFinishBookingResponse> Handle(GetBookingDetailReadyForFinishBookingQuery request, CancellationToken cancellationToken)
     {
+        // B0. Tạo khóa
+        string courtSubdivisionId = request.CourtSubdivisionId.ToString();
+        string dayWantToPlay = request.DayWantToPlay.ToString("yyyyMMdd");
+        string startTimeWantToPlay = request.StartTimeWantToPlay.ToString(@"hh\:mm");
+        string lockKey = $"booking:{courtSubdivisionId}:{dayWantToPlay}:{startTimeWantToPlay}:lock";
+        string lockValue = Guid.NewGuid().ToString();
+        TimeSpan expiry = TimeSpan.FromMinutes(2); // Khóa trong 2 phút
         // B1. Kiểm tra xem sân nhỏ muốn đặt tại thời điểm muốn chơi có trùng với time checking hay không?
-        var courtSub = await _dbContext.CourtSubdivisions.Where(x => x.Id == request.CourtSubdivisionId).SingleOrDefaultAsync();
-        if (courtSub == null)
+        using (var redisLock = new RedisLock(_database, lockKey, lockValue, expiry))
         {
-            throw new NotFoundException("Không tìm thấy sân nào có Id: " + request.CourtSubdivisionId);
-        }
-        var dateTimeStartTimeWantToPlay = request.DayWantToPlay + request.StartTimeWantToPlay;
-        var dateTimeEndTimeWantToPlay = request.DayWantToPlay + request.EndTimeWantToPlay;
-        // Giờ dô time check check nè
-        var timeCheckFlag = await _dbContext.TimeChecking.Where(x => x.CourtSubdivisionId == request.CourtSubdivisionId && !x.IsDelete &&
-                                                                     (
-                                                                     (dateTimeStartTimeWantToPlay <= x.EndTime && dateTimeStartTimeWantToPlay >= x.StartTime) ||
-                                                                     (dateTimeEndTimeWantToPlay <= x.EndTime && dateTimeEndTimeWantToPlay >= x.StartTime) ||
-                                                                     (dateTimeStartTimeWantToPlay <= x.StartTime && dateTimeEndTimeWantToPlay >= x.EndTime)
-                                                                     )).ToListAsync();
-        if (timeCheckFlag.Any())
-        {
-            throw new BadRequestException("Sân bạn muốn đặt đã được đặt trước đó! Vui lòng lựa sân khác nhé");
-        }
-
-        // B2. Lấy các TimePeriods áp dụng cho CourtSubdivisionId
-        var timePeriods = await (from tp in _dbContext.TimePeriods
-                                 join tpcs in _dbContext.TimePeriodCourtSubdivisions
-                                 on tp.Id equals tpcs.TimePeriodId
-                                 where tpcs.CourtSubdivisionId == request.CourtSubdivisionId
-                                 orderby tp.IsNormalDay, tp.StartDayApply
-                                 select tp).ToListAsync();
-
-        var court = await _dbContext.Courts.Where(x => x.Id == courtSub.CourtId).SingleOrDefaultAsync();
-        var response = new BookingDetailReadyForFinishBookingResponse()
-        {
-            CourtId = courtSub.CourtId,
-            CourtName = court.CourtName,
-            ImageCourtURL = court.CourtAvatarImgUrls,
-        };
-        var listCourtSubInReponse = new List<CourtDetailInBookingDetailReadyForFinishBookingReponse>();
-        decimal totalPrice = 0;
-        TimeSpan currentStart = request.StartTimeWantToPlay;
-
-        // Kiểm tra các TimePeriods có IsNormalDay = false trước
-        var specialDayPeriods = timePeriods.Where(tp => !tp.IsNormalDay &&
-                                                        tp.StartDayApply <= request.DayWantToPlay &&
-                                                        tp.EndDayApply >= request.DayWantToPlay).ToList();
-
-        // Áp dụng các TimePeriod đặc biệt
-        foreach (var item in specialDayPeriods)
-        {
-            totalPrice += CalculatePriceForTimePeriod(item, ref currentStart, request, courtSub, listCourtSubInReponse);
-        }
-
-        // Nếu còn thời gian chơi mà chưa được xử lý bởi các TimePeriod đặc biệt
-        if (currentStart < request.EndTimeWantToPlay)
-        {
-            // Kiểm tra các TimePeriods có IsNormalDay = true
-            var weekday = (int)request.DayWantToPlay.DayOfWeek;
-            var normalDayPeriods = timePeriods.Where(tp => tp.IsNormalDay && tp.ListDayByString.Split(',').Contains(weekday.ToString())).ToList();
-
-            foreach (var item in normalDayPeriods)
+            if (redisLock.AcquireLock())
             {
-                totalPrice += CalculatePriceForTimePeriod(item, ref currentStart, request, courtSub, listCourtSubInReponse);
-            }
-
-            // Nếu vẫn còn thời gian chơi chưa được xử lý
-            if (currentStart < request.EndTimeWantToPlay)
-            {
-                var newCourtSubResponse = new CourtDetailInBookingDetailReadyForFinishBookingReponse()
+                try
                 {
-                    TimePeriodId = null,
-                    PriceInTimePeriod = courtSub.BasePrice,
-                    TimePeriodDescription = "Khung giờ từ "
-                                            + currentStart.ToString(@"hh\:mm")
-                                            + " đến "
-                                            + request.EndTimeWantToPlay.ToString(@"hh\:mm")
-                };
-                listCourtSubInReponse.Add(newCourtSubResponse);
+                    // B1. Kiểm tra xem sân nhỏ muốn đặt tại thời điểm muốn chơi có trùng với time checking hay không?
+                    var courtSub = await _dbContext.CourtSubdivisions.Where(x => x.Id == request.CourtSubdivisionId).SingleOrDefaultAsync();
+                    if (courtSub == null)
+                    {
+                        throw new NotFoundException("Không tìm thấy sân nào có Id: " + request.CourtSubdivisionId);
+                    }
+                    var dateTimeStartTimeWantToPlay = request.DayWantToPlay + request.StartTimeWantToPlay;
+                    var dateTimeEndTimeWantToPlay = request.DayWantToPlay + request.EndTimeWantToPlay;
 
-                var timePlayInThisPeriod = request.EndTimeWantToPlay - currentStart;
-                totalPrice += courtSub.BasePrice * Convert.ToDecimal(timePlayInThisPeriod.TotalHours);
+                    // Giờ dô time check check nè
+                    var timeCheckFlag = await _dbContext.TimeChecking.Where(x => x.CourtSubdivisionId == request.CourtSubdivisionId && !x.IsDelete &&
+                                                                         (
+                                                                         (dateTimeStartTimeWantToPlay <= x.EndTime && dateTimeStartTimeWantToPlay >= x.StartTime) ||
+                                                                         (dateTimeEndTimeWantToPlay <= x.EndTime && dateTimeEndTimeWantToPlay >= x.StartTime) ||
+                                                                         (dateTimeStartTimeWantToPlay <= x.StartTime && dateTimeEndTimeWantToPlay >= x.EndTime)
+                                                                         )).ToListAsync();
+                    if (timeCheckFlag.Any())
+                    {
+                        throw new BadRequestException("Sân bạn muốn đặt đã được đặt trước đó! Vui lòng lựa sân khác nhé");
+                    }
+
+                    // B2. Lấy các TimePeriods áp dụng cho CourtSubdivisionId
+                    var timePeriods = await (from tp in _dbContext.TimePeriods
+                                             join tpcs in _dbContext.TimePeriodCourtSubdivisions
+                                             on tp.Id equals tpcs.TimePeriodId
+                                             where tpcs.CourtSubdivisionId == request.CourtSubdivisionId
+                                             orderby tp.IsNormalDay, tp.StartDayApply
+                                             select tp).ToListAsync();
+
+                    var court = await _dbContext.Courts.Where(x => x.Id == courtSub.CourtId).SingleOrDefaultAsync();
+                    var response = new BookingDetailReadyForFinishBookingResponse()
+                    {
+                        CourtId = courtSub.CourtId,
+                        CourtName = court.CourtName,
+                        ImageCourtURL = court.CourtAvatarImgUrls,
+                    };
+                    var listCourtSubInReponse = new List<CourtDetailInBookingDetailReadyForFinishBookingReponse>();
+                    decimal totalPrice = 0;
+                    TimeSpan currentStart = request.StartTimeWantToPlay;
+
+                    // Kiểm tra các TimePeriods có IsNormalDay = false trước
+                    var specialDayPeriods = timePeriods.Where(tp => !tp.IsNormalDay &&
+                                                                    tp.StartDayApply <= request.DayWantToPlay &&
+                                                                    tp.EndDayApply >= request.DayWantToPlay).ToList();
+
+                    // Áp dụng các TimePeriod đặc biệt
+                    foreach (var item in specialDayPeriods)
+                    {
+                        totalPrice += CalculatePriceForTimePeriod(item, ref currentStart, request, courtSub, listCourtSubInReponse);
+                    }
+
+                    // Nếu còn thời gian chơi mà chưa được xử lý bởi các TimePeriod đặc biệt
+                    if (currentStart < request.EndTimeWantToPlay)
+                    {
+                        // Kiểm tra các TimePeriods có IsNormalDay = true
+                        var weekday = (int)request.DayWantToPlay.DayOfWeek;
+                        var normalDayPeriods = timePeriods.Where(tp => tp.IsNormalDay && tp.ListDayByString.Split(',').Contains(weekday.ToString())).ToList();
+
+                        foreach (var item in normalDayPeriods)
+                        {
+                            totalPrice += CalculatePriceForTimePeriod(item, ref currentStart, request, courtSub, listCourtSubInReponse);
+                        }
+
+                        // Nếu vẫn còn thời gian chơi chưa được xử lý
+                        if (currentStart < request.EndTimeWantToPlay)
+                        {
+                            var newCourtSubResponse = new CourtDetailInBookingDetailReadyForFinishBookingReponse()
+                            {
+                                TimePeriodId = null,
+                                PriceInTimePeriod = courtSub.BasePrice,
+                                TimePeriodDescription = "Khung giờ từ "
+                                                        + currentStart.ToString(@"hh\:mm")
+                                                        + " đến "
+                                                        + request.EndTimeWantToPlay.ToString(@"hh\:mm")
+                            };
+                            listCourtSubInReponse.Add(newCourtSubResponse);
+
+                            var timePlayInThisPeriod = request.EndTimeWantToPlay - currentStart;
+                            totalPrice += courtSub.BasePrice * Convert.ToDecimal(timePlayInThisPeriod.TotalHours);
+                        }
+                    }
+
+                    response.ListCourtByTimePeriod = listCourtSubInReponse;
+                    response.TotalPrice = Math.Round(totalPrice, 2);
+                    return response;
+                }
+                finally
+                {
+                    redisLock.ReleaseLock();
+                }
+            }
+            else
+            {
+                throw new BadRequestException("Sân và khung giờ bạn muốn đặt đã được khóa bởi người khác. Vui lòng thử lại sau.");
             }
         }
-
-        response.ListCourtByTimePeriod = listCourtSubInReponse;
-        response.TotalPrice = Math.Round(totalPrice, 2);
-        return response;
     }
 
     private decimal CalculatePriceForTimePeriod(
