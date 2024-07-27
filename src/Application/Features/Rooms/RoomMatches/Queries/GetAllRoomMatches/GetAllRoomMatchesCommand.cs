975c6c4fea483b1cf416768d8fa339f5a591613d
minhncn <minh27122002pro@gmail.com>
2024-07-27 20:47:48 +0700

    [BeatSports][270724][2047][Add][Add 3 filter in get all room match api]

--- src/Application/Features/Rooms/RoomMatches/Queries/GetAllRoomMatches/GetAllRoomMatchesCommand.cs
+++ src/Application/Features/Rooms/RoomMatches/Queries/GetAllRoomMatches/GetAllRoomMatchesCommand.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using BeatSportsAPI.Application.Common.Models;
 using BeatSportsAPI.Application.Common.Response;
+using BeatSportsAPI.Domain.Enums;
 using MediatR;
 
 namespace BeatSportsAPI.Application.Features.Rooms.RoomMatches.Queries.GetAllRoomMatches;
@@ -16,4 +17,9 @@ public class GetAllRoomMatchesCommand : IRequest<RoomRequestsResponseForGetAll>
     //[Required]
     //public int PageSize { get; set; }
     public Guid CustomerId { get; set; }
+    public string? Level { get; set; }
+    //Query để search cho tên chủ phòng và địa chỉ
+    public string? Query { get; set; }
+    [EnumDataType(typeof(SportCategoriesEnums))]
+    public SportCategoriesEnums sportFilter { get; set; }
 }
