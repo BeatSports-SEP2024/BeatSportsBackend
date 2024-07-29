5270b5ffc71fb5aa5e4cc7ee851efddfa5b126b1
vinnt060402 <vinnt060402@gmail.com>
2024-07-29 23:26:42 +0700

    [BeatSports][290724][2326][Add] Update API Invoice

--- src/Application/Features/Bookings/Queries/GetBookingFinishForInvoice/GetBookingFinishForInvoiceQuery.cs
+++ src/Application/Features/Bookings/Queries/GetBookingFinishForInvoice/GetBookingFinishForInvoiceQuery.cs
@@ -1,7 +1,13 @@
 ﻿namespace BeatSportsAPI.Application.Features.Bookings.Queries.GetBookingFinishForInvoice;
+/// <summary>
+/// Get hết những th booking của court id này theo ngày bắt đầu, ngày kết thúc mà FE gửi xuống
+/// </summary>
 public class GetBookingFinishForInvoiceQuery
 {
     public Guid CourtId { get; set; }
+    /// <summary>
+    /// trong so sánh nhớ có >= hoặc <=, đừng dùng mỗi dấu > <
+    /// </summary>
     public DateTime DayStart { get; set; }
     public DateTime DayEnd { get; set; }
 }
@@ -9,26 +15,33 @@ public class BookingFinishForInvoiceResponse
 {
     /// <summary>
     /// Count ngày
+    /// Int i++ theo ngày bởi vì kh có cố định
     /// </summary>
     public int IdFlag { get; set; }
     /// <summary>
     /// Ngày check
+    /// Format ra string yyyy-MM-dd hh:mm:ss
     /// </summary>
-    public string DateCheck { get; set; }
+    public string? DateCheck { get; set; }
+    public List<BookingOfCourtInDay>? ListBooked { get; set; }
 }
 public class BookingOfCourtInDay
 {
     public Guid BookingId { get; set; }
+    public Guid CourtSubdivision { get; set; }
+    public Guid CustomerId { get; set; }
+    /// <summary>
+    /// Full name của người chơi
+    /// </summary>
+    public string? FullNameOfCustomer { get; set; }
+    public string? CourtSubdivisionName { get; set; }
     /// <summary>
     /// Ngày đặt
+    /// Format ra string yyyy-MM-dd hh:mm:ss
     /// </summary>
-    public string DayTimeBooking { get; set; }
+    public string? DayTimeBooking { get; set; }
     /// <summary>
     /// Tổng giá tiền
     /// </summary>
     public decimal TotalPrice { get; set; }
-    /// <summary>
-    /// Full name của người chơi
-    /// </summary>
-    public string FullNameOfUser { get; set; }
 }
