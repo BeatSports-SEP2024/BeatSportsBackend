139ed2717219d73f00d837ba63e9e99db6bb444a
minhncn <minh27122002pro@gmail.com>
2024-07-31 17:43:49 +0700

    [BeatSports][310724][1743][CherryPick][Pick from Dev-nhan bracnh]

--- src/Domain/Entities/Transaction.cs
+++ src/Domain/Entities/Transaction.cs
@@ -13,6 +13,12 @@ public class Transaction : BaseAuditableEntity
     public decimal? TransactionAmount { get; set; }
     public DateTime? TransactionDate { get; set; }    
     public string? TransactionType { get; set; } 
+    /// <summary>
+    /// Không cần relationship, để 
+    /// check coi call back được thì lấy 
+    /// danh sách nạp tiền lên cho customer
+    /// </summary>
+    public string? PaymentTransactionId{ get; set; }
 
     public virtual Wallet Wallet { get; set; } = null!;
     public virtual Booking Booking { get; set; }
