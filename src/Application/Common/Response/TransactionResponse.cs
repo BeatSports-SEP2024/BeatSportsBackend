3784f5ba6f43b64ddd703eb8c08a11fc0499f523
minhncn <minh27122002pro@gmail.com>
2024-08-01 01:48:55 +0700

    [BeatSports][010824][0107][Add][Add API GetAllWithdrawalRequestByOwner]

--- src/Application/Common/Response/TransactionResponse.cs
+++ src/Application/Common/Response/TransactionResponse.cs
@@ -22,10 +22,21 @@ public class TransactionResponse : IMapFrom<Transaction>
     public string? TransactionType { get; set; }
 }
 
-public class TransactionResponseV2 
+public class TransactionResponseV3 
 {
     public Guid TransactionId { get; set; }
-    public UserInfo From { get; set; }  
+    public UserInfo2 UserInfo { get; set; }  
+    public decimal? TransactionAmount { get; set; }
+    public string? TransactionStatus { get; set; }
+    public string AdminCheckStatus { get; set; }
+    public DateTime? TransactionDate { get; set; }
+    public string? TransactionType { get; set; }
+}
+
+public class TransactionResponseV2
+{
+    public Guid TransactionId { get; set; }
+    public UserInfo From { get; set; }
     public UserInfo To { get; set; }
     public decimal? TransactionAmount { get; set; }
     public string? TransactionStatus { get; set; }
