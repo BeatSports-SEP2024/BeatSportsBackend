f24d5452ebea2f82f193765584fe0e64ab905995
minhncn <minh27122002pro@gmail.com>
2024-08-01 19:41:33 +0700

    [BeatSports][010824][1941][Add][Add paginated list]

--- src/Application/Features/Transactions/Queries/GetAllWithdrawalRequestByOwner/GetAllWithdrawalRequestByOwnerCommand.cs
+++ src/Application/Features/Transactions/Queries/GetAllWithdrawalRequestByOwner/GetAllWithdrawalRequestByOwnerCommand.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,8 +8,12 @@ using BeatSportsAPI.Application.Common.Response;
 using MediatR;
 
 namespace BeatSportsAPI.Application.Features.Transactions.Queries.GetAllWithdrawalRequestByOwner;
-public class GetAllWithdrawalRequestByOwnerCommand : IRequest<List<TransactionResponseV3>>
+public class GetAllWithdrawalRequestByOwnerCommand : IRequest<PaginatedTransactionResponseV2>
 {
+    [Required]
+    public int PageIndex { get; set; }
+    [Required]
+    public int PageSize { get; set; }   
     public string? KeyWord { get; set; }
     public string? Filter { get; set; }
     public DateTime? StartTime { get; set; }
