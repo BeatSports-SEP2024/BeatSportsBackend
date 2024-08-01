420bcb30bb94d1ab8e5c1e574fafcaaf07c8e3d0
minhncn <minh27122002pro@gmail.com>
2024-08-01 15:25:28 +0700

    [BeatSports][010824][1512][Add][define status]

--- src/Domain/Entities/Transaction.cs
+++ src/Domain/Entities/Transaction.cs
@@ -8,7 +8,18 @@ public class Transaction : BaseAuditableEntity
     public Guid WalletTargetId { get; set; }
     public string? TransactionMessage { get; set; }
     public string? TransactionPayload { get; set; }
+    /// <summary>
+    /// TransactionStatus: Trạng thái này theo dõi quá trình xử lý của giao dịch, 
+    /// chẳng hạn như việc trừ tiền khỏi ví của khách hàng đã hoàn tất hay chưa, 
+    /// và nó phản ánh trạng thái hiện tại của giao dịch như đã hoàn thành, đang chờ xử lý, thất bại, hay đã được hoàn trả.
+    /// </summary>
     public string? TransactionStatus { get; set; }
+    /// <summary>
+    /// AdminCheckStatus: Trạng thái này liên quan đến quan điểm của chủ sân (owner), 
+    /// Trạng thái này cực kỳ quan trọng để phân biệt liệu giao dịch có nằm trong khoảng thời gian mà khách hàng có thể hủy hay không, 
+    /// giúp quản lý các tình huống cần giữ giao dịch tạm thời trước khi hoàn tất cho tài khoản của chủ sân, 
+    /// đảm bảo việc xử lý các hủy bỏ tiềm năng một cách thích hợp.
+    /// </summary>
     public AdminCheckEnums? AdminCheckStatus { get; set; }
     public decimal? TransactionAmount { get; set; }
     public DateTime? TransactionDate { get; set; }    
