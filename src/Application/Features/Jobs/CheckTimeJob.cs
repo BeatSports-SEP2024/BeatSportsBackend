0be2cd07afe0964ee6cf06908d2608ab52e91605
ducbinh02 <ducbinhnguyen.2002@gmail.com>
2024-08-02 02:38:42 +0700

    [BeatSports][020824][0237][Add][Add CronJob to check BookingTime]

--- src/Application/Features/Jobs/CheckTimeJob.cs
+++ src/Application/Features/Jobs/CheckTimeJob.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BeatSportsAPI.Application.Common.Exceptions;
 using BeatSportsAPI.Application.Common.Interfaces;
 using BeatSportsAPI.Domain.Enums;
 
@@ -46,5 +47,52 @@ public class CheckTimeJob
                 }
             }
         }
+    }
+
+    public void CheckTimeOfBooking()
+    {
+        var bookingList = _beatSportsDbContext.Bookings
+                        .Where(x => !x.IsDelete && x.BookingStatus.Equals("Pending"))
+                        .ToList();
+
+        foreach (var booking in bookingList)
+        {
+            // Chuyển đổi Unix timestamp ngược lại thành DateTime
+            // DateTimeOffset dateTimeOffsetFromUnix = DateTimeOffset.FromUnixTimeSeconds(bookingApprove.UnixTimestampMinCancellation);
+            DateTime datetimeFromUnix = booking.UnixTimestampMinCancellation;
+            // Ghép PlayingDate và StartTimePlaying lại
+            DateTime playingStartDateTime = booking.PlayingDate.Date.Add(booking.StartTimePlaying - datetimeFromUnix.TimeOfDay);
+
+            // Lấy thời gian hiện tại
+            DateTime currentDateTime = DateTime.Now;
+
+            // So sánh thời gian hủy với thời gian hiện tại
+            TimeSpan timeDifference = playingStartDateTime - currentDateTime;
+
+            if (timeDifference <= TimeSpan.Zero)
+            {
+                // Thời gian hủy nhỏ hơn hoặc bằng thời gian hiện tại
+                //throw new BadRequestException($"Không thể hủy đặt sân, thời gian tối thiểu để hủy lịch đặt sân đã trôi qua. Thời gian bị lệch: {timeDifference}");
+                var transaction = _beatSportsDbContext.Transactions
+                                .Where(x => x.Id == booking.TransactionId && x.AdminCheckStatus == AdminCheckEnums.Pending)
+                                .FirstOrDefault();
+                if(transaction != null)
+                {
+                    var ownerWallet = _beatSportsDbContext.Wallets
+                                .Where(x => x.Id == transaction.WalletTargetId)
+                                .FirstOrDefault();
+
+                    transaction.AdminCheckStatus = AdminCheckEnums.Accepted;
+
+                    ownerWallet.Balance += (int)transaction.TransactionAmount;
+
+                    _beatSportsDbContext.Transactions.Update(transaction);
+                    _beatSportsDbContext.Wallets.Update(ownerWallet);
+                    _beatSportsDbContext.SaveChanges();
+                }
+                
+            }
+        }
+        
     }
 }
