1c484f1bbb916737f244ac27d68dde07f10fe740
minhncn <minh27122002pro@gmail.com>
2024-08-02 00:16:23 +0700

    [BeatSports][020824][0016][Add][Add api get transaction by owner]

---
+++ src/Application/Features/Transactions/Queries/GetAllTransactionByOwner/GetAllTransactionByOwnerHandler.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BeatSportsAPI.Application.Common.Interfaces;
+using BeatSportsAPI.Application.Common.Mappings;
+using BeatSportsAPI.Application.Common.Models;
+using BeatSportsAPI.Application.Common.Response;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace BeatSportsAPI.Application.Features.Transactions.Queries.GetAllTransactionByOwner;
+public class GetAllTransactionByOwnerHandler : IRequestHandler<GetAllTransactionByOwner, PaginatedList<TransactionResponse>>
+{
+    private readonly IBeatSportsDbContext _beatSportsDbContext;
+
+    public GetAllTransactionByOwnerHandler(IBeatSportsDbContext beatSportsDbContext)
+    {
+        _beatSportsDbContext = beatSportsDbContext;
+    }
+
+    public async Task<PaginatedList<TransactionResponse>> Handle(GetAllTransactionByOwner request, CancellationToken cancellationToken)
+    {
+        var query = _beatSportsDbContext.Transactions
+            .Include(t => t.Wallet)
+                .ThenInclude(w => w.Account)
+                    .ThenInclude(a => a.Owner)
+            .Where(t => t.Wallet.Account.Owner.Id == request.OwnerId && t.TransactionType == "Giao dịch trong ứng dụng");
+
+        var response = await query.Select(t => new TransactionResponse
+        {
+            TransactionId = t.Id,
+            WalletId = t.WalletId,
+            WalletTargetId = t.WalletTargetId,
+            TransactionMessage = t.TransactionMessage,
+            TransactionPayload = t.TransactionPayload,
+            TransactionStatus = t.TransactionStatus,
+            AdminCheckStatus = t.AdminCheckStatus.ToString(),
+            TransactionAmount = t.TransactionAmount,
+            TransactionDate = t.TransactionDate,
+            TransactionType = t.TransactionType
+        }).PaginatedListAsync(request.PageIndex, request.PageSize);
+       
+        return response;
+    }
+}
