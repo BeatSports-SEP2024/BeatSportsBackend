2643968487a8f9b11eab66e0348fe3e7048b71b5
chim347 <nhanntse160792@gmail.com>
2024-08-04 23:12:26 +0700

    [BeatSports][040824][2312][Update][Update SignalR in roomRequest]

---
+++ src/Application/Features/Hubs/RoomRequestHub.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BeatSportsAPI.Application.Features.Hubs;
+public class RoomRequestHub : Hub
+{
+    public async Task UpdateRoom(string action, string customerId)
+    {
+        await Clients.Group(Context.ConnectionId).SendAsync("UpdateRoom", action, customerId);
+    }
+
+    public async Task JoinRoomGroup(string roomId)
+    {
+        await Groups.AddToGroupAsync(Context.ConnectionId, roomId);
+    }
+
+    public async Task LeaveRoomGroup(string roomId)
+    {
+        await Groups.RemoveFromGroupAsync(Context.ConnectionId, roomId);
+    }
+
+    public async Task UpdateMemberStatus(string roomRequestId, string customerId, string status)
+    {
+        await Clients.Group(roomRequestId).SendAsync("UpdateMemberStatus", customerId, status);
+    }
+}
